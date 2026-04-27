9c5ce5248c599125e978bba0d6f0ab4300ad70db
Abhishek Singh <abhisheksingh2@csod.com>
2026-04-27 11:38:19 +0530

    updated generatejob

--- jobs/generatedJobs.groovy
+++ jobs/generatedJobs.groovy
@@ -1,3 +1,11 @@
+folder("${REPO_NAME}") {
+    description("Jobs for ${REPO_NAME}")
+}
+
+folder("${REPO_NAME}/${ENV}") {
+    description("${ENV} jobs for ${REPO_NAME}")
+}
+
 pipelineJob("${REPO_NAME}/${ENV}/${JOB_NAME}") {
     description("Generated job for ${JOB_NAME}")
 
